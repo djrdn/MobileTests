13baaf9615dd5f04983e9aa77a40660c00d1ac2e
Dmytro <dimandinchik@gmail.com>
2025-06-15 01:14:32 +0300

    write config files for test

--- src/test/java/config/ConfigReader.java
+++ src/test/java/config/ConfigReader.java
@@ -1,4 +1,14 @@
 package config;
 
+import org.aeonbits.owner.ConfigFactory;
+
 public class ConfigReader {
+    /**
+     * Читалка для emulator.properties
+     */
+    public static final EmulatorConfig emulatorConfig = ConfigFactory.create(EmulatorConfig.class, System.getProperties());
+    /**
+     * Читалка для test.properties
+     */
+    public static final TestConfig testConfig = ConfigFactory.create(TestConfig.class, System.getProperties());
 }
